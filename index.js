0b570c7cc3f99429ffbeee40628a5829f42acb09
huynhquangdev <141423816+Quangqq@users.noreply.github.com>
2025-01-19 01:53:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-var http = require('http');
-var request = require('request');
+var request = require('request')
 var cron = require('node-cron');
-var fs = require('fs');
 
-var data = fs.readFileSync('input.txt', 'utf8').toString().split('\n');
+var fs = require("fs"); 
+var data = fs.readFileSync('input.txt','utf8').toString().split('\n');
 
 var i = 1;
-data.forEach(function (value) {
-    cron.schedule('*/1 * * * * *', () => {
-        request(value, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("Phiên thứ: " + i++);
-                console.log("Response: " + body);
-                console.log("--------------------------");
-            }
-        });
+data.forEach(function(value){
+    cron.schedule('*/1 * * * * * *', () => {
+      request(value, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Phiên thứ: "+i++);
+            console.log("Response: "+body);
+            console.log("--------------------------");
+        }
+    })
     });
-});
-
-// Mở một cổng để giữ ứng dụng chạy
-const PORT = 3000;
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Server is running\n');
-}).listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
 });
